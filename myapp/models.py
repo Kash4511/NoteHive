4882ba051f4b9003c720c3ef22020be493e1f7d5
Kaashif <Kaashifameen32@gmail.com>
2024-12-04 22:15:42 +0530

    Tailwind hot reload,models and migrations

--- myapp/models.py
+++ myapp/models.py
@@ -1,2 +1,15 @@
+from django.db import models
+from django.utils import timezone
 
+class varity(models.Model):
+    Choice = [
+        ('SH', 'Shoe'),
+        ('CL', 'Cloths'),
+        ('WT', 'Watch'),
+
+    ]
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='app')
+    date_added = models.DateTimeField(default=timezone.now)
+    type = models.CharField(max_length=2,choices=Choice)
 
--- practice/urls.py
+++ practice/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-# from django.conf import settings
-# from django.conf.urls.static import static
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
@@ -12,9 +12,7 @@ urlpatterns = [
     path('myapp/', include('myapp.urls')),
 
     path("__reload__/", include("django_browser_reload.urls")),
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-]
 
 
-   
-# ] + static(settings.MEDIA.URL, document_root=settings.MEDIA_ROOT)
