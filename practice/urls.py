1217a4bd2f179abe11116db6c438dab1810216f2
Kaashif <Kaashifameen32@gmail.com>
2025-01-16 21:44:21 +0530

    about and contact linked and funtionable

--- practice/urls.py
+++ practice/urls.py
@@ -7,6 +7,8 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('contact/', views.contact, name='contact'),
+    path('about/', views.about, name='about'),
     path('index/', views.index, name='index'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/register/', views.registration_view, name='register'),
