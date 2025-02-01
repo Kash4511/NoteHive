e70082c912200c60503c127167b2bfa8560c8d7f
Kaashif <Kaashifameen32@gmail.com>
2025-02-01 19:19:45 +0530

    notes list on dashboard

--- notes/urls.py
+++ notes/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import notes_list_view, create_note_view, delete_note_view
 
 urlpatterns = [
-    path('', notes_list_view, name='notes_list'),
+    path('list/', notes_list_view, name='notes_list'),
     path('new/', create_note_view, name='create_note'),
     path('delete/<int:note_id>/', delete_note_view, name='delete_note'),
 ]
